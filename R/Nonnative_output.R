7b619c87a198fd0d17db57e57d4165c7b2dacea9
Kerry Flaherty Walia <kfwalia@tbep.org>
2026-02-12 12:49:18 -0500

    code and output for nonnative species from TB FIM data

---
+++ R/Nonnative_output.R
@@ -0,0 +1,30 @@
+###Subset all FIM data from GEI project data to just Tampa Bay
+
+# Load required libraries
+library(dplyr)
+library(tidyr)
+library(readr)
+library(lubridate)
+library(stringr)
+library(here)
+library(usethis)
+library(tbeptools)
+
+load(here("Output", "tb_all_c.RData"))
+
+#Export species list
+spp <- com %>%
+  group_by(Scientificname,Commonname,species)%>%
+  summarise(total_count = sum(number, na.rm = TRUE),
+            n_observations=n())
+write_csv(spp, here("Output", "tb_fim_species.csv"))
+#add any new non-natives, invasives to fim_nonnative_list.csv
+
+invlist <- read_csv(here("Output","fim_nonnative_list.csv"))%>%
+  select(Scientificname,Commonname,Taxa,Taxa_group,Taxa_Type,nonnative,invasive)
+         
+#filter by non-native, invasive species records
+inv <- full_join(com,invlist, by ="Scientificname")%>%
+  filter(nonnative=="Y")
+
+
